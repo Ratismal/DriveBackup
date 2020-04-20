8b9c07b7c7f49003fea74fd8e71af55344042ca5
BuildTools <unconfigured@null.spigotmc.org>
2020-04-20 08:07:35 +0200

    decrease thread prioroity for backup

--- src/main/java/ratismal/drivebackup/UploadThread.java
+++ src/main/java/ratismal/drivebackup/UploadThread.java
@@ -10,7 +10,6 @@ import ratismal.drivebackup.util.*;
 import ratismal.drivebackup.util.Timer;
 
 import java.io.File;
-import java.text.DecimalFormat;
 import java.util.*;
 
 /**
@@ -41,80 +40,89 @@ public class UploadThread implements Runnable {
      */
     @Override
     public void run() {
-        if (PlayerListener.doBackups || forced) {
-            MessageUtil.sendMessageToAllPlayers(Config.getBackupStart());
-            // Create Backup Here
-            HashMap<String, HashMap<String, Object>> backupList = Config.getBackupList();
-            for (Map.Entry<String, HashMap<String, Object>> set : backupList.entrySet()) {
 
-                String type = set.getKey();
-                String format = set.getValue().get("format").toString();
-                String create = set.getValue().get("create").toString();
+        try {
+            Thread.currentThread().setPriority(Thread.NORM_PRIORITY - 2);
 
-                List<String> blackList = new ArrayList<>();
-                if (set.getValue().containsKey("blacklist")) {
-                    Object tempObject = set.getValue().get("blacklist");
-                    if (tempObject instanceof List<?>) {
-                        blackList = (List<String>) tempObject;
-                    }
-                }
+            if (PlayerListener.doBackups || forced) {
+                MessageUtil.sendMessageToAllPlayers(Config.getBackupStart());
+                // Create Backup Here
+                HashMap<String, HashMap<String, Object>> backupList = Config.getBackupList();
+                for (Map.Entry<String, HashMap<String, Object>> set : backupList.entrySet()) {
 
-                MessageUtil.sendConsoleMessage("Doing backups for " + type);
-                if (create.equalsIgnoreCase("true")) {
-                    FileUtil.makeBackup(type, format, blackList);
-                }
+                    String type = set.getKey();
+                    String format = set.getValue().get("format").toString();
+                    String create = set.getValue().get("create").toString();
 
-                File file = FileUtil.getFileToUpload(type, format, false);
-                ratismal.drivebackup.util.Timer timer = new Timer();
-                try {
-                    if (Config.isGoogleEnabled()) {
-                        MessageUtil.sendConsoleMessage("Uploading file to GoogleDrive");
-                        timer.start();
-                        GoogleUploader.uploadFile(file, type);
-                        timer.end();
-                        MessageUtil.sendConsoleMessage(timer.getUploadTimeMessage(file));
-                    }
-                    if (Config.isOnedriveEnabled()) {
-                        MessageUtil.sendConsoleMessage("Uploading file to OneDrive");
-                        //Couldn't get around static issue, declared a new Instance.
-                        OneDriveUploader oneDrive = new OneDriveUploader();
-                        timer.start();
-                        oneDrive.uploadFile(file, type);
-                        timer.end();
-                        MessageUtil.sendConsoleMessage(timer.getUploadTimeMessage(file));
-                    }
-                    if (Config.isFtpEnabled()) {
-                        MessageUtil.sendConsoleMessage("Uploading file to FTP");
-                        timer.start();
-                        FTPUploader.uploadFile(file, type);
-                        timer.end();
-                        MessageUtil.sendConsoleMessage(timer.getUploadTimeMessage(file));
-                    }
-
-                    if (!Config.keepLocalBackup()) {
-                        if (file.delete()) {
-                            MessageUtil.sendConsoleMessage("Old backup deleted.");
-                        } else {
-                            MessageUtil.sendConsoleMessage("Failed to delete backup " + file.getAbsolutePath());
+                    List<String> blackList = new ArrayList<>();
+                    if (set.getValue().containsKey("blacklist")) {
+                        Object tempObject = set.getValue().get("blacklist");
+                        if (tempObject instanceof List<?>) {
+                            blackList = (List<String>) tempObject;
                         }
                     }
-                    //MessageUtil.sendConsoleMessage("File Uploaded.");
-                } catch (Exception e) {
-                    if (Config.isDebug())
-                        e.printStackTrace();
+
+                    MessageUtil.sendConsoleMessage("Doing backups for " + type);
+                    if (create.equalsIgnoreCase("true")) {
+                        FileUtil.makeBackup(type, format, blackList);
+                    }
+
+                    File file = FileUtil.getFileToUpload(type, format, false);
+                    ratismal.drivebackup.util.Timer timer = new Timer();
+                    try {
+                        if (Config.isGoogleEnabled()) {
+                            MessageUtil.sendConsoleMessage("Uploading file to GoogleDrive");
+                            timer.start();
+                            GoogleUploader.uploadFile(file, type);
+                            timer.end();
+                            MessageUtil.sendConsoleMessage(timer.getUploadTimeMessage(file));
+                        }
+                        if (Config.isOnedriveEnabled()) {
+                            MessageUtil.sendConsoleMessage("Uploading file to OneDrive");
+                            // Couldn't get around static issue, declared a new Instance.
+                            OneDriveUploader oneDrive = new OneDriveUploader();
+                            timer.start();
+                            oneDrive.uploadFile(file, type);
+                            timer.end();
+                            MessageUtil.sendConsoleMessage(timer.getUploadTimeMessage(file));
+                        }
+                        if (Config.isFtpEnabled()) {
+                            MessageUtil.sendConsoleMessage("Uploading file to FTP");
+                            timer.start();
+                            FTPUploader.uploadFile(file, type);
+                            timer.end();
+                            MessageUtil.sendConsoleMessage(timer.getUploadTimeMessage(file));
+                        }
+
+                        if (!Config.keepLocalBackup()) {
+                            if (file.delete()) {
+                                MessageUtil.sendConsoleMessage("Old backup deleted.");
+                            } else {
+                                MessageUtil.sendConsoleMessage("Failed to delete backup " + file.getAbsolutePath());
+                            }
+                        }
+                        // MessageUtil.sendConsoleMessage("File Uploaded.");
+                    } catch (Exception e) {
+                        if (Config.isDebug())
+                            e.printStackTrace();
+                    }
                 }
-            }
-            if (forced) {
-                MessageUtil.sendMessageToAllPlayers(Config.getBackupDone());
+                if (forced) {
+                    MessageUtil.sendMessageToAllPlayers(Config.getBackupDone());
+                } else {
+                    MessageUtil.sendMessageToAllPlayers(Config.getBackupDone() + " " + Config.getBackupNext()
+                            .replaceAll("%TIME", String.valueOf(Config.getBackupDelay() / 20 / 60)));
+                }
+                if (Bukkit.getOnlinePlayers().size() == 0 && PlayerListener.doBackups) {
+                    MessageUtil.sendConsoleMessage("Disabling automatic backups due to inactivity.");
+                    PlayerListener.doBackups = false;
+                }
             } else {
-                MessageUtil.sendMessageToAllPlayers(Config.getBackupDone() + " " + Config.getBackupNext().replaceAll("%TIME", String.valueOf(Config.getBackupDelay() / 20 / 60)));
+                MessageUtil.sendConsoleMessage("Skipping backup.");
             }
-            if (Bukkit.getOnlinePlayers().size() == 0 && PlayerListener.doBackups) {
-                MessageUtil.sendConsoleMessage("Disabling automatic backups due to inactivity.");
-                PlayerListener.doBackups = false;
-            }
-        } else {
-            MessageUtil.sendConsoleMessage("Skipping backup.");
+
+        } catch (Exception e) {
+            MessageUtil.sendConsoleMessage("ERROR: Something went wrong in UploadThread:" + e.getMessage());
         }
     }
 
